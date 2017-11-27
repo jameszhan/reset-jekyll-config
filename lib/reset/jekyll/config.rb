234c094296e1caefda058356d86c79f5c9ed7083
James Zhan <zhiqiangzhan@gmail.com>
2017-11-27 17:26:18 +0800

    Initialize Gem

---
+++ lib/reset/jekyll/config.rb
@@ -0,0 +1,9 @@
+require "reset/jekyll/config/version"
+
+module Reset
+  module Jekyll
+    module Config
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/reset/jekyll/config/version.rb
@@ -0,0 +1,7 @@
+module Reset
+  module Jekyll
+    module Config
+      VERSION = "0.1.0"
+    end
+  end
+end
